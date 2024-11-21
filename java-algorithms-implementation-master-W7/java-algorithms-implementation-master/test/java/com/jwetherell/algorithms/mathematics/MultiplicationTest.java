43991762049af33432b42b5e361cdb72f4f73b3e
Tacitor <lukasjkrampitz@gmail.com>
2024-11-20 19:51:59 -0500

    Inital commit

---
+++ java-algorithms-implementation-master-W7/java-algorithms-implementation-master/test/java/com/jwetherell/algorithms/mathematics/MultiplicationTest.java
@@ -0,0 +1,41 @@
+package com.jwetherell.algorithms.mathematics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiplicationTest {
+
+    @Test
+    void multiplication() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    void multiplyUsingLoop() {
+    }
+
+    @Test
+    void multiplyUsingRecursion() {
+    }
+
+    @Test
+    void multiplyUsingShift() {
+    }
+
+    @Test
+    void multiplyUsingLogs() {
+    }
+
+    @Test
+    void multiplyUsingFFT() {
+    }
+
+    @Test
+    void multiplyUsingLoopWithStringInput() {
+    }
+
+    @Test
+    void multiplyUsingLoopWithIntegerInput() {
+    }
+}
---
+++ java-algorithms-implementation-master-W7/java-algorithms-implementation-master/test/java/com/jwetherell/algorithms/mathematics/PrimesTest.java
@@ -0,0 +1,24 @@
+package com.jwetherell.algorithms.mathematics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PrimesTest {
+
+    @Test
+    void getPrimeFactorization() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    void isPrime() {
+    }
+
+    @Test
+    void sieveOfEratosthenes() {
+    }
+
+    @Test
+    void millerRabinTest() {
+    }
+}
